b6ef6187674284bffae7843084ca46be4238ef7a
Annihilous <benjaminjnorton@gmail.com>
2016-01-14 20:06:00 -0500

    Create page 6 and blackout dates modal.

--- partials/_navigation.php
+++ partials/_navigation.php
@@ -1,3 +1,10 @@
+<ul>     
+  <li><a href="/shelby_arena/pcc-connect-update-2-01.php">pcc-connect-update-2-01.php</a></li>
+  <li><a href="/shelby_arena/pcc-connect-update-2-04.php">pcc-connect-update-2-04.php</a></li>
+  <li><a href="/shelby_arena/pcc-connect-update-2-05.php">pcc-connect-update-2-05.php</a></li>
+  <li><a href="/shelby_arena/pcc-connect-update-2-06.php">pcc-connect-update-2-06.php</a></li>
+</ul>
+
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -9,14 +16,12 @@
         <span class="icon-bar"></span>
       </button>
     </div>
-
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="/shelby_arena/pcc-connect-update-2-01.php">pcc-connect-update-2-01.php</a></li>
-        <li><a href="/shelby_arena/pcc-connect-update-2-04.php">pcc-connect-update-2-04.php</a></li>
-        <li><a href="/shelby_arena/pcc-connect-update-2-05.php">pcc-connect-update-2-05.php</a></li>
-        <li><a href="#" data-toggle="modal" data-target="#editProfileModal" class="text-blue">Edit Profile</a></li>
+        <li><a href="#" data-toggle="modal" data-target="#editProfileModal" class="text-blue">Profile Modal</a></li>
+        <li><a href="#" data-toggle="modal" data-target="#signUpModal" class="text-blue">Sign Up Modal</a> </li>
+        <li><a href="#" data-toggle="modal" data-target="#blackoutDatesModal" class="text-blue">Blackout DatesModal</a> </li>
       </ul>
 
     </div><!-- /.navbar-collapse -->
@@ -24,3 +29,5 @@
 </nav>
 
 <?php include("partials/_edit_profile_modal.php"); ?>
+<?php include("partials/_sign_up_modal.php"); ?>
+<?php include("partials/_blackout_dates_modal.php"); ?>
---
+++ pcc-connect-update-2-06.php
@@ -0,0 +1,111 @@
+<?php include("partials/_head.php"); ?>
+
+<body>
+  <?php include("partials/_navigation.php"); ?>
+
+  <div class="flexslider">
+    <ul class="slides">
+      <li>
+        <img src="https://lh3.ggpht.com/l8leWV9uI8gS-cKDaak6D__cSCVanRmsAGqNRDpXutowAjRRoEmnuhmbTP5A9exBz1w=h900" />
+      </li>
+      <li>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZ-McKOtm6Wijwy5onK4MtcUVe7CndFLeAIzh_e3hjpTt3gKT" />
+      </li>
+      <li>
+        <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTvI5aHKF9q5YFgNo1dvYIERaey7pjuErzgMNQEJrV0NkQgBA-t" />
+      </li>
+    </ul>
+  </div>
+
+  <div class="background-white">
+    <div class="pad-section pad-section-sm container container-md">
+      <div class="row">
+        <div class="col-sm-8">
+          <h4><b>My Schedule</b></h4>
+          <table class="table">
+            <tr class="background-gray text-white">
+              <th colspan="5">Sun, Dec 13 - Sunday Gatherings</th>
+            </tr>
+            <tr class="background-lightgray">
+              <td><b>10:00am</b></td>
+              <td></td>
+              <td></td>
+              <td><span><b>Team Name -</b></span>&nbsp Role Requested</td> 
+              <td class="pull-right"><i>7:45am arrival</i></td>
+            </tr>
+            <tr class="background-darkgray">
+              <td><b>5:00pm</b></td>
+              <td></td>
+              <td></td>
+              <td><span><b>Team Name -</b></span>&nbsp Role Requested</td> 
+              <td class="pull-right"><i>4:00pm arrival</i></td>
+            </tr>
+            <tr class="background-gray text-white">
+              <th colspan="5">Tue, Dec 15 - Special Event</th>
+            </tr>
+            <tr class="background-lightgray">
+              <td><b>7:00pm</b></td>
+              <td></td>
+              <td></td>
+              <td><span><b>Team Name -</b></span>&nbsp Role Requested</td> 
+              <td class="pull-right"><i>5:30pm arrival</i></td>
+            </tr>
+          </table>
+        </div>
+        <div class="col-sm-4">
+          <div id="servingScheduleCalendar">
+            <div class="text-center">
+              <p style="padding: 120px 0px">Calendar Placeholder</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </br>
+
+
+      <hr>
+
+      <div class="row">
+        <div class="col-xs-12 team-heading">
+          <p class="pull-right">contact: team@passioncitychurch.com</p>
+          <p>Team Name 1</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="http://guyharveysportswear.com/blog/wp-content/uploads/2012/09/GH-Artist-three.jpg">
+        </div>
+        <div class="col-sm-4">
+          <img src="http://www.claymoorslist.com/wp-content/uploads/blogger/-X6NVGF2Qiio/T5ZHqCx4zhI/AAAAAAAAF_w/Ht1CUpVxxBA/s640/sdfsfsdf.JPG">
+        </div>
+        <div class="col-sm-4">
+          <img src="http://www.unep.org/resourcepanel/Portals/50244/images/nivo-images/slide2.jpg">
+        </div>
+        <div class="col-xs-12 team-heading">
+          <p class="pull-right">contact: team@passioncitychurch.com</p>
+          <p>Team Name 2</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="http://www.unep.org/resourcepanel/Portals/50244/images/nivo-images/slide2.jpg">
+        </div>
+        <div class="col-sm-4">
+          <img src="http://guyharveysportswear.com/blog/wp-content/uploads/2012/09/GH-Artist-three.jpg">
+        </div>
+        <div class="col-sm-4">
+          <img src="http://www.claymoorslist.com/wp-content/uploads/blogger/-X6NVGF2Qiio/T5ZHqCx4zhI/AAAAAAAAF_w/Ht1CUpVxxBA/s640/sdfsfsdf.JPG">
+        </div>
+        <div class="col-xs-12 team-heading">
+          <p class="pull-right">contact: team@passioncitychurch.com</p>
+          <p>Team Name 3</p>
+        </div>
+        <div class="col-sm-4">
+          <img src="http://guyharveysportswear.com/blog/wp-content/uploads/2012/09/GH-Artist-three.jpg">
+        </div>
+        <div class="col-sm-4">
+          <img src="http://www.claymoorslist.com/wp-content/uploads/blogger/-X6NVGF2Qiio/T5ZHqCx4zhI/AAAAAAAAF_w/Ht1CUpVxxBA/s640/sdfsfsdf.JPG">
+        </div>
+        <div class="col-sm-4">
+          <img src="http://www.unep.org/resourcepanel/Portals/50244/images/nivo-images/slide2.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
