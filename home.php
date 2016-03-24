4f82ecfa77be4ac73a53c8bac190573f4bd98d19
Annihilous <benjaminjnorton@gmail.com>
2016-03-24 12:31:05 -0400

    WIP: Lunch Timegit stgit stgit st :-)

--- home.php
+++ home.php
@@ -5,8 +5,8 @@
   <?php include("partials/_header.php"); ?>
 
   <section>
-    <img src="https://lh3.ggpht.com/l8leWV9uI8gS-cKDaak6D__cSCVanRmsAGqNRDpXutowAjRRoEmnuhmbTP5A9exBz1w=h900" />
-    <img src="assets/images/design.png" />
+    <img src="https://lh3.ggpht.com/l8leWV9uI8gS-cKDaak6D__cSCVanRmsAGqNRDpXutowAjRRoEmnuhmbTP5A9exBz1w=h900">
+    <img src="assets/images/design.png">
   </section>
 
   <?php include("partials/_watch_modal.php"); ?>
